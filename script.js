f29a45e0b244b6dd9ae2f5afbadc3a24f9baf3dc
ingrihoegset <43383570+ingrihoegset@users.noreply.github.com>
2023-10-15 23:18:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const outputDueDate = document.getElementById('dueDateResult');
     const outputExpectedChildren = document.getElementById('expectedChildrenResult');
     const outputLeavePayPercentage = document.getElementById('leavePayPercentageResult');
+    const outputWeeksVacationMother = document.getElementById('weeksVacationMother');
+    const outputWeeksVacationFather = document.getElementById('weeksVacationFather');
+    const outputWeeksVacationTogether = document.getElementById('weeksVacationTogether');
+    const outputTotalWeeksOfVacation = document.getElementById('totalWeeksOfVacation');
     const outputPermStart = document.getElementById('permStartResult');
     const outputPermEnd = document.getElementById('permEndResult');
     const calculateButton = document.getElementById('calculateButton');
@@ -12,27 +16,40 @@ document.addEventListener('DOMContentLoaded', function () {
         const selectedDueDate = document.getElementById('dueDate').value;
         const selectedExpectedChildren = parseInt(document.getElementById('expectedChildren').value);
         const selectedLeavePayPercentage = parseInt(document.getElementById('leavePayPercentage').value);
+        const selctedVacationMother = parseInt(document.getElementById('vacationMother').value);
+        const selctedVacationFather = parseInt(document.getElementById('vacationFather').value);
+        const selctedVacationTogether = parseInt(document.getElementById('vacationTogether').value);
         const calculatedPermStart = calculateTotalPermStart(selectedDueDate);
-        const calculatedPermEnd = calculateTotalPermEnd(calculatedPermStart, selectedExpectedChildren, selectedLeavePayPercentage);
+        const calculatedVacationWeeks = calculateVacationWeeks(selctedVacationMother, selctedVacationFather, selctedVacationTogether)
+        const calculatedPermEnd = calculateTotalPermEnd(calculatedPermStart, selectedExpectedChildren, selectedLeavePayPercentage, calculatedVacationWeeks);
 
-        outputDueDate.textContent = `Due date: ${selectedDueDate}`;
-        outputExpectedChildren.textContent = `Expected Children: ${selectedExpectedChildren}`;
-        outputLeavePayPercentage.textContent = `Leave pay percentage: ${selectedLeavePayPercentage}`;
+        outputDueDate.textContent = `Termin: ${selectedDueDate}`;
+        outputExpectedChildren.textContent = `Antall barn: ${selectedExpectedChildren}`;
+        outputLeavePayPercentage.textContent = `Andel foreldrepenger: ${selectedLeavePayPercentage}`;
+        outputWeeksVacationMother.textContent = `Ferie bare mor: ${selctedVacationMother}`;
+        outputWeeksVacationFather.textContent = `Ferie bare far / medmor: ${selctedVacationFather}`;
+        outputWeeksVacationTogether.textContent =`Ferie sammen: ${selctedVacationTogether}`;
+        outputTotalWeeksOfVacation.textContent =`Totalt antall ferieuker: ${calculatedVacationWeeks}`;
         outputPermStart.textContent = `Permisjonen starter: ${calculatedPermStart.toLocaleDateString()}`;
         outputPermEnd.textContent = `Permisjonen slutter: ${calculatedPermEnd.toLocaleDateString()}`;
 
         resultSection.classList.remove('hidden');
     });
+
+
 });
+
+
 
 function calculateTotalPermStart(inputDate) {
     const calculatedStartDate = new Date(inputDate);
     calculatedStartDate.setDate(calculatedStartDate.getDate() - 21); // Subtract 3 weeks (3 * 7 days)
-    return calculatedStartDate
+    return calculatedStartDate;
 }
 
-function calculateTotalPermEnd(startDate, expectedChildren, leavePayPercentage) {
+function calculateTotalPermEnd(startDate, expectedChildren, leavePayPercentage, weeksOfVacation) {
     var totalWeeksOfLeave = 0;
+    var totalWeeksOfVacation = 0;
 
     // Case where leave pay percentage is selected to be 100%
     if (leavePayPercentage === 100) {
@@ -59,7 +76,33 @@ function calculateTotalPermEnd(startDate, expectedChildren, leavePayPercentage) 
         }
     }
 
+    //Check if vacation weeks is calculated
+    if (!isNaN(weeksOfVacation)) {
+        totalWeeksOfVacation = weeksOfVacation
+    } else {
+        totalWeeksOfVacation = 0
+    }
+
     const calculatedPermEnd = new Date(startDate);
+    // Add weeks of paid leave
     calculatedPermEnd.setDate(calculatedPermEnd.getDate() + totalWeeksOfLeave * 7)
+    // Add weeks of vacation
+    calculatedPermEnd.setDate(calculatedPermEnd.getDate() + totalWeeksOfVacation * 7)
     return calculatedPermEnd;
 }
+
+function calculateVacationWeeks(vacationMother, vacationFather, vacationTogether) {
+    return vacationMother + vacationFather + vacationTogether
+}
+
+
+// Slider indicator
+const slider = document.getElementById('myRange');
+const output = document.getElementById('sliderValue');
+
+output.innerHTML = slider.value; // Display the default value
+
+slider.addEventListener('input', function() {
+    output.innerHTML = this.value;
+});
+
