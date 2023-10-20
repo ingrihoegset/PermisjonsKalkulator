970cfe66740afa5bcc76ebdad9196468f6381fa9
ingrihoegset <43383570+ingrihoegset@users.noreply.github.com>
2023-10-20 13:47:03 +0200

    Add files via upload

--- foreldre.js
+++ foreldre.js
@@ -72,7 +72,7 @@ class Mor extends Foreldre {
         if (!isNaN(nyTermin)) {
             this._termin = nyTermin;
             console.log('Termindato for mor ble oppdatert til ' + this._termin.toISOString().split('T')[0]);
-            this.oppdaterRettigheter(this._termin, this._antallBarn, this._rettighetsKlasse, this._andelPenger);
+            this.oppdaterRettigheter();
         } else {
             console.log('Ugyldig datoformat for ny termin.');
         }
@@ -82,7 +82,7 @@ class Mor extends Foreldre {
     setAndelPenger(oppgittAndel) {
         if (Number.isInteger(oppgittAndel)) {
             this._andelPenger = oppgittAndel;
-            this.oppdaterRettigheter(this._termin, this._antallBarn, this._rettighetsKlasse, this._andelPenger);
+            this.oppdaterRettigheter();
         } else {
             console.error('Invalid input for andel foreldrepenger. Please provide an Integer.');
         }
@@ -93,17 +93,106 @@ class Mor extends Foreldre {
         if (Number.isInteger(nyKlasse)) {
             this._rettighetsKlasse = nyKlasse;
             console.log('Rettighetsklasse for Mor oppdatert til ' + nyKlasse);
-            this.oppdaterRettigheter(this._termin, this._antallBarn, this._rettighetsKlasse, this._andelPenger);
+            this.oppdaterRettigheter();
         } else {
             console.error('Invalid input for rettighetsklasse. Please provide an Integer.');
         }
     }
 
+    oppdaterRettigheter() {
+        console.log('Oppdaterer rettigheter for Mor med termin: ' + this._termin + ' Rettighetsklasse: ' + this._rettighetsKlasse + ' Andel penger: ' + this._andelPenger);
 
-    oppdaterRettigheter(termin, antallBarn, rettighetsKlasse, andelPenger) {
-        console.log('Oppdaterer rettigheter for Mor med termin: ' + termin + ' Rettighetsklasse: ' + rettighetsKlasse + ' Andel penger: ' + andelPenger);
+        if (this._andelPenger === 100) {
+            console.log('foreldrepenger er ' + this._andelPenger + ' %'); 
+            // Rettighetsklasse 1
+            if(this._rettighetsKlasse === 1) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 15;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 2) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 5;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 3) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 46;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 4) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 46;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 5) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 0;
+                this._harRettigheter = true;
+            }
+            else {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 0;
+                this._ukerMorKvote = 0;
+                this._harRettigheter = false;
+            }
+        }
+        else if (this._andelPenger === 80) {
+            console.log('foreldrepenger er ' + this._andelPenger + ' %'); 
+            // Rettighetsklasse 1
+            if(this._rettighetsKlasse === 1) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 19;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 2) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 5;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 3) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 56;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 4) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 56;
+                this._harRettigheter = true;
+            }
+            else if(this._rettighetsKlasse === 5) {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 3;
+                this._ukerMorKvote = 0;
+                this._harRettigheter = true;
+            }
+            else {
+                console.log('Fant rettighetsklasse ' + this._rettighetsKlasse); 
+                this._ukerForFodsel = 0;
+                this._ukerMorKvote = 0;
+                this._harRettigheter = false;
+            }
+        }
+        else {
+            console.error('Invalid input for foreldrepenger')
+        }
+
+        // Beregn permisjon
+        beregnPermisjon()
     }
 
+    beregnPermisjon() {
+        console.log('Beregner permisjon med rettighetene: har rettigheter ' + this._harRettigheter + ' Uker etter fødsel til mor: ' + this._ukerMorKvote + ' Uker før fødsel til mor: ' + this._ukerForFodsel);
+    }
 }
 
 
